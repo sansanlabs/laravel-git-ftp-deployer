bebd088259da54f104e9f50c8265db6a5864cbb0
Edi Kurniawan <edikurniawan.dev@gmail.com>
2026-04-18 10:20:40 +0700

    chore: :fire: remove version

--- src/GitFtpDeployer.php
+++ src/GitFtpDeployer.php
@@ -4,11 +4,6 @@ namespace SanSanLabs\GitFtpDeployer;
 
 class GitFtpDeployer
 {
-    public function version(): string
-    {
-        return '1.0.0';
-    }
-
     public function getAvailableEnvironments(): array
     {
         return array_keys(config('git-ftp-deployer.environments', []));
