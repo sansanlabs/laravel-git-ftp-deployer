eaab0853c65c92fa39f3a803b06dc84791367f6e
Edi Kurniawan <edikurniawan.dev@gmail.com>
2026-04-18 10:16:27 +0700

    style: :art: formatting

--- src/GitFtpDeployer.php
+++ src/GitFtpDeployer.php
@@ -2,16 +2,20 @@
 
 namespace SanSanLabs\GitFtpDeployer;
 
-class GitFtpDeployer {
-  public function version(): string {
-    return "1.0.0";
-  }
+class GitFtpDeployer
+{
+    public function version(): string
+    {
+        return '1.0.0';
+    }
 
-  public function getAvailableEnvironments(): array {
-    return array_keys(config("git-ftp-deployer.environments", []));
-  }
+    public function getAvailableEnvironments(): array
+    {
+        return array_keys(config('git-ftp-deployer.environments', []));
+    }
 
-  public function getEnvironmentConfig(string $environment): ?array {
-    return config("git-ftp-deployer.environments.{$environment}");
-  }
+    public function getEnvironmentConfig(string $environment): ?array
+    {
+        return config("git-ftp-deployer.environments.{$environment}");
+    }
 }
